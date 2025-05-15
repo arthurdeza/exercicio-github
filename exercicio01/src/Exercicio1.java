f1369bcdb07987206babfa592abc20b77328c9bd
arthurdeza <marciadasilvadezanet@gmail.com>
2025-05-15 16:18:09 -0300

    add exercicio01

---
+++ exercicio01/src/Exercicio1.java
@@ -0,0 +1,22 @@
+
+  import javax.swing.JOptionPane;
+
+public class Exercicio1 {
+    public static boolean ehPerfeito(int numero) {
+        int soma = 0;
+        for (int i = 1; i < numero; i++) {
+            if (numero % i == 0) {
+                soma += i;
+            }
+        }
+        return soma == numero;
+    }
+
+    public static void main(String[] args) {
+        int numero = Integer.parseInt(JOptionPane.showInputDialog("Digite um número:"));
+        JOptionPane.showMessageDialog(null, numero + (ehPerfeito(numero) ? " é perfeito." : " não é perfeito."));
+    }
+}
+  
+    
+
